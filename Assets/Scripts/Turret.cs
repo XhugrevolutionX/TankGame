f7eaab797721b6d9bbce30acbd6d1994778ac2b3
hugo Zeidan <hugzeidan@outlook.com>
2024-12-05 00:05:53 +0100

    Turret Rotate

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -3,20 +3,27 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class Turret : MonoBehaviour
 {
-    private TurretDetectionZone _turretDetectionZone = null;
+
     
     [SerializeField] private float fireRate = 1f;
+    [SerializeField] private float _headRotateSpeed = 50;
     [SerializeField] private Projectile bullet;
     [SerializeField] private Transform firePoint0;
     [SerializeField] private Transform firePoint1;
     [SerializeField] private Transform firePoint2;
-    [SerializeField] private Transform firePoint3;
-    private bool shoot_on;
+    [SerializeField] private Transform firePoint3; 
+    private bool _shootOn;
+    private TurretDetectionZone _turretDetectionZone = null;
+    private GameObject _head;
+    private float _headAngleY;
+    private bool _headRotationDir = false;
+    private bool _headStopRotation = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _turretDetectionZone = transform.GetChild(1).transform.GetChild(2).transform.GetChild(4).transform.GetChild(0).GetComponent<TurretDetectionZone>();
+        _turretDetectionZone = transform.GetChild(1).GetChild(2).GetChild(4).GetChild(0).GetComponent<TurretDetectionZone>();
+        _head = transform.GetChild(1).GetChild(2).gameObject;
     }
 
     // Update is called once per frame
@@ -24,21 +31,68 @@ public class Turret : MonoBehaviour
     {
         if (_turretDetectionZone._isOn)
         {
-            if(!shoot_on)
+            StartCoroutine("DetectionDelay");
+            
+            if(!_shootOn)
             {
                 StartCoroutine("Fire");
             }
         }
         else
         {
-            shoot_on = false;
+            _shootOn = false;
+            _headStopRotation = false;
+            StopCoroutine("DetectionDelay");
             StopCoroutine("Fire");
+        }
+
+        if (!_headStopRotation)
+        {
+            //Get the Y angle of the head 
+            if (_head.transform.eulerAngles.y <= 180f)
+            {
+                _headAngleY = _head.transform.eulerAngles.y;
+            }
+            else
+            {
+                _headAngleY = _head.transform.eulerAngles.y - 360f;
+            }
+
+            //Make the head rotate
+            if (_headAngleY >= 90)
+            {
+                _headRotationDir = true;
+            }
+
+            if (_headAngleY <= -90)
+            {
+                _headRotationDir = false;
+            }
+
+            if (_headRotationDir == false)
+            {
+                _head.transform.Rotate(0, _headRotateSpeed * Time.deltaTime, 0);
+            }
+            else
+            {
+                _head.transform.Rotate(0, -_headRotateSpeed * Time.deltaTime, 0);
+            }
+        }
+    }
+
+    IEnumerator DetectionDelay()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.2f);
+            
+            _headStopRotation = true;
         }
     }
     
     IEnumerator Fire()
     {
-        shoot_on = true;
+        _shootOn = true;
         while (true)
         {
             Instantiate(bullet, firePoint0.position, firePoint0.rotation);
